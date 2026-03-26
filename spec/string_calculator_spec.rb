270b3069246431aeb3b9eb9bc999c72fc67b44e3
kavya-2 <kavya.sivapal@gmail.com>
2026-03-26 15:54:20 +0530

    Test 3: Two numbers return sum - Red-Green cycle complete

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,15 +4,21 @@ RSpec.describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
   describe '#add' do
-    context 'when input is empty string' do
+    context 'when input is a empty string' do
       it 'returns 0' do
         expect(calculator.add("")).to eq(0)
       end
     end
 
-    context 'when input is single number' do
-      it 'returns the number' do
+    context 'when input is a single number' do
+      it 'returns the same number' do
         expect(calculator.add("1")).to eq(1)
+      end
+    end
+
+    context 'when input is two numbers' do
+      it 'returns sum of numbers' do
+        expect(calculator.add("1,5")).to eq(6)
       end
     end
   end
