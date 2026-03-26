94979eb0c66b934dda1a4bf0ef93204328c6bee2
kavya-2 <kavya.sivapal@gmail.com>
2026-03-26 15:57:31 +0530

    Test 6: Support custom delimiters - Red-Green cycle complete

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,15 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+    
+    delimiter = /,|\n/
+    if numbers.start_with?('//')
+      delimiter_line, numbers_string = numbers.split("\n", 2)
+      custom_delimiter = delimiter_line[2..-1]
+      delimiter = Regexp.escape(custom_delimiter)
+      numbers = numbers_string
+    end
+    
+    numbers.split(delimiter).map(&:to_i).sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -33,5 +33,11 @@ RSpec.describe StringCalculator do
         expect(calculator.add("1\n2,3")).to eq(6)
       end
     end
+
+    context 'when input has custom delimiter' do
+      it 'handles custom delimiter syntax' do
+        expect(calculator.add("//;\n1;2")).to eq(3)
+      end
+    end
   end
 end
