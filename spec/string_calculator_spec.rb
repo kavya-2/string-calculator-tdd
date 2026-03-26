1cad3ea6ebe85916e13fd4b1829535dd0c913515
kavya-2 <kavya.sivapal@gmail.com>
2026-03-26 15:54:51 +0530

    Test 4: Handle any amount of numbers - Test passes with existing implementation

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,5 +21,11 @@ RSpec.describe StringCalculator do
         expect(calculator.add("1,5")).to eq(6)
       end
     end
+
+    context 'when input is multiple numbers' do
+      it 'returns sum of all numbers' do
+        expect(calculator.add("1,2,3,4,5")).to eq(15)
+      end
+    end
   end
 end
